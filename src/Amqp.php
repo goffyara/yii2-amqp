f62cabbf16daede28040f19ef927e0c6df21adfd
Shelepov G <gvshelepov@gmail.com>
2017-10-18 17:53:12 +0300

    initial commit

---
+++ src/Amqp.php
@@ -0,0 +1,80 @@
+<?php
+namespace goffyara\amqp\components;
+
+use yii\base\Component;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class Amqp extends Component
+{
+    public $host;
+    public $port;
+    public $user;
+    public $password;
+
+    private $_channel;
+    private $_connection;
+
+    public function getConnection() {
+
+        if (is_object($this->_connection)) {
+            return $this->_connection;
+        }
+
+        $this->_connection = new AMQPStreamConnection(
+            $this->host,
+            $this->port,
+            $this->user,
+            $this->password
+        );
+
+        return $this->_connection;
+    }
+
+    public function getChannel() {
+
+        if (is_object($this->_channel)) {
+            return $this->_channel;
+        }
+
+        $this->_channel = $this->getConnection()->channel();
+        return $this->_channel;
+    }
+
+    public function getMessage(string $data , array $properties = []) {
+
+        $defaultProperties = [
+            'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
+        ];
+
+        $properties = array_merge($defaultProperties, $properties);
+
+        return new AMQPMessage($data, $properties);
+    }
+
+    public function publish(AMQPMessage $message, string $key, string $exchange = '')
+    {
+        $this->channel->basic_publish($message, $exchange, $key);
+    }
+
+    public function nack(AMQPMessage $message)
+    {
+        return $message
+            ->delivery_info['channel']
+            ->basic_nack($message->delivery_info['delivery_tag']);
+    }
+
+    public function ack(AMQPMessage $message)
+    {
+        return $message
+            ->delivery_info['channel']
+            ->basic_ack($message->delivery_info['delivery_tag']);
+    }
+
+    public function requeue(AMQPMessage $message)
+    {
+        $message
+            ->delivery_info['channel']
+            ->basic_nack($message->delivery_info['delivery_tag'], false, true);
+    }
+}
